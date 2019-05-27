6ed91bcbf9db215d8492b0a6ae15c1678cb90292
CK <ckhonglogin@163.com>
2019-05-28 00:59:32 +0800

    algorithm

---
+++ CodeLib-2017/Assets/Scripts/AlgorithmTool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlgorithmTool  
+{	
+	public static void BubbleSort(int[] array)
+	{
+		for(int i = 0; i < array.Length; i++)
+		{
+			bool finish = true;
+			for(int j = 0; j < array.Length - i - 1; j ++)
+			{
+				if(array[j] > array[j+1])
+				{
+					var temp = array[j];
+					array[j] = array[j+1];
+					array[j+1] = temp;
+					finish = false;
+				}
+			}
+			if(finish) break;
+		}
+	}
+}
