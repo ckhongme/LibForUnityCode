7076d2894ec6c48d35a242250d8b49c8688494de
CK <ckhonglogin@163.com>
2019-12-17 23:06:28 +0800

    增减算法Test脚本

---
+++ CodeLib-2017/Assets/Project/SampleScript/AlgorithmTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using K;
+using System;
+
+public class AlgorithmTest : MonoBehaviour
+{
+    private int[] ARRAY = new int[10] { 6, 1, 2, 7, 4, 3, 9, 5, 10, 8 };
+
+    void Start()
+    {
+        _TestQuickSort();
+    }
+
+    #region Test BubbleSort
+
+    private void _TestBubbleSort()
+    {
+
+    }
+
+
+    #endregion
+
+    #region Test QuickSort
+
+    private void _TestQuickSort()
+    {
+        var array = new int[10];
+        var time = 0f;
+
+        time = Time.realtimeSinceStartup;
+        ARRAY.CopyTo(array, 0);
+        AlgorithmTool.QuickSort(array, 0, array.Length - 1);
+        DebugArray("QuickSort", array, time);
+
+        time = Time.realtimeSinceStartup;
+        ARRAY.CopyTo(array, 0);
+        AlgorithmTool.QuickSort2(array, 0, array.Length - 1);
+        DebugArray("QuickSort2", array, time);
+
+    }
+
+    private void DebugArray(string funName, int[] array, float time)
+    {
+        time = Time.realtimeSinceStartup - time;
+        StringBuilder sb = new StringBuilder();
+        foreach (var item in array)
+        {
+            sb.Append(item);
+            sb.Append(",");
+        }
+        Debug.Log(funName + ": " + sb.ToString() + " - usingTime: " + time);
+    }
+
+    #endregion
+}
+
+
--- CodeLib-2017/Assets/Scripts/AlgorithmTool.cs
+++ CodeLib-2017/Assets/Scripts/AlgorithmTool.cs
@@ -2,24 +2,103 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AlgorithmTool  
-{	
-	public static void BubbleSort(int[] array)
-	{
-		for(int i = 0; i < array.Length; i++)
-		{
-			bool finish = true;
-			for(int j = 0; j < array.Length - i - 1; j ++)
-			{
-				if(array[j] > array[j+1])
-				{
-					var temp = array[j];
-					array[j] = array[j+1];
-					array[j+1] = temp;
-					finish = false;
-				}
-			}
-			if(finish) break;
-		}
-	}
+namespace K
+{
+    public class AlgorithmTool
+    {
+        #region 冒泡排序 
+
+        /// <summary>
+        /// 冒泡排序（从小到大排序）
+        /// </summary>
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool finish = true;
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        var temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        finish = false;
+                    }
+                }
+                if (finish) break;
+            }
+        }
+
+        #endregion
+
+        #region 快速排序
+
+        public static void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition(array, left, right);
+                QuickSort(array, left, pivot - 1);
+                QuickSort(array, pivot + 1, right);
+            }
+        }
+
+        private static int Partition(int[] array, int left, int right)
+        {
+            int pivot = left;
+            while (left < right)
+            {
+                while (left < right && array[right] >= array[pivot])
+                    right--;
+                while (left < right && array[left] <= array[pivot])
+                    left++;
+                Swap(array, left, right);
+            }
+            Swap(array, pivot, left);
+            return left;
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (array.Length > j & array.Length > i)
+            {
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+
+
+        public static void QuickSort2(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = Partition2(array, left, right);
+                QuickSort2(array, left, pivot - 1);
+                QuickSort2(array, pivot + 1, right);
+            }
+        }
+
+        private static int Partition2(int[] array, int left, int right)
+        {
+            int pivot = array[left];
+            while (left < right)
+            {
+                while (left < right && array[right] >= pivot)
+                    right--;
+                array[left] = array[right];
+
+                while (left < right && array[left] <= pivot)
+                    left++;
+                array[right] = array[left];
+            }
+            array[left] = pivot;
+            return left;
+        }
+
+        #endregion
+
+    }
 }
