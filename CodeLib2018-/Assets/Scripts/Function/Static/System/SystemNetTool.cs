c729666ded4357cd562400134279ab6a46962972
ckhongme <ckhonglogin@163.com>
2020-01-04 18:49:39 +0800

    网卡信息

--- CodeLib2018-/Assets/Scripts/Function/Static/System/SystemNetTool.cs
+++ CodeLib2018-/Assets/Scripts/Function/Static/System/SystemNetTool.cs
@@ -12,43 +12,76 @@ namespace K.system
         /// </summary>
         /// <param name="addressFamily"> IPv4 = InterNetwork,  IPv6 = InterNetworkV6 </param>
         /// <returns></returns>
-        public static string GetIpStr(AddressFamily addressFamily)
+        public static string GetIpStr(AddressFamily addressFamily = AddressFamily.InterNetwork)
         {
-            //Return null if addressFamily is Ipv6 but OS does not support it
-            if (addressFamily == AddressFamily.InterNetworkV6 && !Socket.OSSupportsIPv6)
-            {
-                return null;
-            }
+            var adapter = GetActiveAdapter(addressFamily);
+            if (adapter == null) return null;
 
             string ipStr = "";
+            foreach (UnicastIPAddressInformation ip in adapter.GetIPProperties().UnicastAddresses)
+            {
+                if (ip.Address.AddressFamily == addressFamily)
+                {
+                    ipStr = ip.Address.ToString();
+                    break;
+                }
+            }
+            return ipStr;
+        }
+
+        /// <summary>
+        /// 获取网络适配器的名称
+        /// </summary>
+        /// <param name="addressFamily"></param>
+        /// <returns></returns>
+        public static string GetAdapterName(AddressFamily addressFamily = AddressFamily.InterNetwork)
+        {
+            var adapter = GetActiveAdapter(addressFamily);
+            if (adapter == null) return null;
+            return adapter.Name;
+        }
+
+        /// <summary>
+        /// 获取 MAC 地址
+        /// </summary>
+        /// <param name="addressFamily"></param>
+        /// <returns></returns>
+        public static string GetMAC(AddressFamily addressFamily = AddressFamily.InterNetwork)
+        {
+            var adapter = GetActiveAdapter(addressFamily);
+            if (adapter == null) return null;
+            return adapter.GetPhysicalAddress().ToString();
+        }
+
+        /// <summary>
+        /// 获取网络接口的速度
+        /// </summary>
+        /// <param name="addressFamily"></param>
+        /// <returns></returns>
+        public static string GetSpeed(AddressFamily addressFamily = AddressFamily.InterNetwork)
+        {
+            var adapter = GetActiveAdapter(addressFamily);
+            if (adapter == null) return null;
+            return adapter.Speed.ToString();
+        }
+
+        /// <summary>
+        /// 获取 网络适配器 （接口类型是 Wifi 或者 以太网）
+        /// </summary>
+        /// <param name="addressFamily"> IPv4 = InterNetwork,  IPv6 = InterNetworkV6 </param>
+        /// <returns></returns>
+        public static NetworkInterface GetActiveAdapter(AddressFamily addressFamily)
+        {
             foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
             {
                 //如果接口类型是 Wifi 或者 以太网
                 if (adapter.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 || adapter.NetworkInterfaceType == NetworkInterfaceType.Ethernet)
                 {
                     if (adapter.OperationalStatus == OperationalStatus.Up)
-                    {
-                        foreach (UnicastIPAddressInformation ip in adapter.GetIPProperties().UnicastAddresses)
-                        {
-                            if (ip.Address.AddressFamily == addressFamily)
-                            {
-                                ipStr = ip.Address.ToString();
-                                break;
-                            }
-                        }
-                    }
+                        return adapter;
                 }
             }
-            return ipStr;
+            return null;
         }
-
-
-        //private NetworkInterface _GetAdapter()
-        //{
-
-        //}
-
-
-
     }
 }
